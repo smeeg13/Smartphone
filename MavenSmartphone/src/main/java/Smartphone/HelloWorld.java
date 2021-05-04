49397df1863e5f536a2d4f4a66f77bbc985c3874
thomascheseaux <Sethcohen95>
2021-05-04 10:49:49 +0200

    thomas premier test

--- MavenSmartphone/src/main/java/Smartphone/HelloWorld.java
+++ MavenSmartphone/src/main/java/Smartphone/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Thomas est respectable");
+
+        System.out.println("Mon premier message sur GIT");
     }
 }
