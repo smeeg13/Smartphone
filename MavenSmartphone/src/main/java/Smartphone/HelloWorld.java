6969bffb513da1a9fc222bb426ab1ec028397916
Nathan Dély <nathan.dely@students.hevs.ch>
2021-05-04 10:47:03 +0200

    slt

--- MavenSmartphone/src/main/java/Smartphone/HelloWorld.java
+++ MavenSmartphone/src/main/java/Smartphone/HelloWorld.java
@@ -3,5 +3,6 @@ package Smartphone;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Thomas est respectable");
     }
 }
